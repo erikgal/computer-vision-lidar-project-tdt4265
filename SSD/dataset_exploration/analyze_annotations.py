be8ad0dee62158f3b9c455e3de3e63bbe488fb69
Erik Galler <43548211+erikgal@users.noreply.github.com>
2022-04-01 17:26:11 +0200

    EDA Analysis

---
+++ SSD/dataset_exploration/analyze_annotations.py
@@ -0,0 +1,43 @@
+import torch
+from tops.config import instantiate, LazyConfig
+from ssd import utils
+from tqdm import tqdm
+
+
+def get_config(config_path):
+    cfg = LazyConfig.load(config_path)
+    cfg.train.batch_size = 1
+    return cfg
+
+
+def get_dataloader(cfg, dataset_to_visualize):
+    if dataset_to_visualize == "train":
+        # Remove GroundTruthBoxesToAnchors transform
+        cfg.data_train.dataset.transform.transforms = cfg.data_train.dataset.transform.transforms[:-1]
+        data_loader = instantiate(cfg.data_train.dataloader)
+    else:
+        cfg.data_val.dataloader.collate_fn = utils.batch_collate
+        data_loader = instantiate(cfg.data_val.dataloader)
+
+    return data_loader
+
+
+def analyze_nbr_labels(dataloader, cfg):
+    label_count = torch.zeros(len(cfg.label_map.keys()))
+    for batch in tqdm(dataloader):
+        label_count += torch.bincount(batch['labels'][0], minlength=len(cfg.label_map.keys()))
+    print(label_count)
+
+def main():
+    config_path = "configs/tdt4265.py"
+    cfg = get_config(config_path)
+    dataset_to_analyze = "train"  # or "val"
+
+    print("Label map is:", cfg.label_map)
+
+    dataloader = get_dataloader(cfg, dataset_to_analyze)
+    analyze_nbr_labels(dataloader, cfg)
+
+
+if __name__ == '__main__':
+    main()
